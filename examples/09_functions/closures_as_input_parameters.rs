83c902375bfe0ced14e222391cb3b34ced384a0f
fushouhai <fushouhai@starmerx.com>
2025-07-07 16:52:08 +0800

    add 09

---
+++ examples/09_functions/closures_as_input_parameters.rs
@@ -0,0 +1,69 @@
+// A function which takes a closure as an argument and calls it.
+// <F> denotes that F is a "Generic type parameter"
+fn apply<F>(f: F)
+where
+    // The closure takes no input and returns nothing.
+    F: FnOnce(),
+{
+    // ^ TODO: Try changing this to `Fn` or `FnMut`.
+
+    f();
+}
+
+// A function which takes a closure and returns an `i32`.
+fn apply_to_3<F>(f: F) -> i32
+where
+    // The closure takes an `i32` and returns an `i32`.
+    F: Fn(i32) -> i32,
+{
+    f(3)
+}
+
+fn main() {
+    use std::mem;
+
+    let greeting = "hello";
+    // A non-copy type.
+    // `to_owned` creates owned data from borrowed one
+    let mut farewell = "goodbye".to_owned();
+
+    // Capture 2 variables: `greeting` by reference and
+    // `farewell` by value.
+    let diary = || {
+        // `greeting` is by reference: requires `Fn`.
+        println!("I said {}.", greeting);
+
+        // Mutation forces `farewell` to be captured by
+        // mutable reference. Now requires `FnMut`.
+        farewell.push_str("!!!");
+        println!("Then I screamed {}.", farewell);
+        println!("Now I can sleep. zzzzz");
+
+        // Manually calling drop forces `farewell` to
+        // be captured by value. Now requires `FnOnce`.
+        mem::drop(farewell);
+    };
+
+    // Call the function which applies the closure.
+    apply(diary);
+
+    // `double` satisfies `apply_to_3`'s trait bound
+    let double = |x| 2 * x;
+
+    /*
+        double 是一个简单的闭包，它只是对输入参数进行乘法操作
+
+    这个闭包不捕获任何外部变量（没有使用外部环境的变量）
+         */
+    println!("3 doubled: {}", apply_to_3(double));
+}
+
+/*
+闭包作为参数，对闭包的约束
+Fn: the closure uses the captured value by reference (&T)
+FnMut: the closure uses the captured value by mutable reference (&mut T)
+FnOnce: the closure uses the captured value by value (T)
+
+if FnOnce is possible, then FnMut, or Fn is also ok,
+if FnMut is possible, then Fn is also ok
+*/
