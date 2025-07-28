f006684f979173a42525a901ce294e5f93e9e180
fushouhai <fushouhai@starmerx.com>
2025-07-28 17:25:31 +0800

    add 16

---
+++ examples/16_traits/impl_trait.rs
@@ -0,0 +1,32 @@
+fn parse_csv_document<R: std::io::BufRead>(src: R) -> std::io::Result<Vec<Vec<String>>> {
+    src.lines()
+        .map(|line| {
+            // For each line in the source
+            line.map(|line| {
+                // If the line was read successfully, process it, if not, return the error
+                line.split(',') // Split the line separated by commas
+                    .map(|entry| String::from(entry.trim())) // Remove leading and trailing whitespace
+                    .collect() // Collect all strings in a row into a Vec<String>
+            })
+        })
+        .collect() // Collect all lines into a Vec<Vec<String>>
+}
+
+// fn parse_csv_document(src: impl std::io::BufRead) -> std::io::Result<Vec<Vec<String>>> {
+//     src.lines()
+//         .map(|line| {
+//             // For each line in the source
+//             line.map(|line| {
+//                 // If the line was read successfully, process it, if not, return the error
+//                 line.split(',') // Split the line separated by commas
+//                     .map(|entry| String::from(entry.trim())) // Remove leading and trailing whitespace
+//                     .collect() // Collect all strings in a row into a Vec<String>
+//             })
+//         })
+//         .collect() // Collect all lines into a Vec<Vec<String>>
+// }
+
+fn main() {
+    let _ = parse_csv_document::<std::io::Empty>(std::io::empty());
+    // not work in fn parse_csv_document(src: impl std::io::BufRead) -> std::io::Result<Vec<Vec<String>>>
+}
